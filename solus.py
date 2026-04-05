07ed489c266bf3ccd3f698de0dea335d32ce9519
dave_ <supermariocrafter05@gmail.com>
2026-04-05 17:08:28 +0100

    v0.2.0

--- solus.py
+++ solus.py
@@ -1,4 +1,4 @@
-# SOLUS by DESYNTAX - VERSION v0.1.1 - CREATED 24/02/26 - LAST UPDATED 05/04/26
+# SOLUS by DESYNTAX - VERSION v0.2.0 - CREATED 24/02/26 - LAST UPDATED 05/04/26
 print("Starting CLI...")
 dangerousProceed = ""
 sty = {
